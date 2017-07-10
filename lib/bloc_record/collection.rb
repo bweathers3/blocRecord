c657a70964550ff5a53541418125dd1d93d42aa2
Bailey <bailey.g.weathers@gmail.com>
2017-07-09 19:57:19 -0500

    Complete 05-assignment-update without the take method

--- lib/bloc_record/collection.rb
+++ lib/bloc_record/collection.rb
@@ -5,6 +5,14 @@ module BlocRecord
       ids = self.map(&:id)
       self.any? ? self.first.class.update(ids, updates) : false
     end
-    
+
+    def where(args)
+      multi_wheres = []
+      for key in args.keys
+        multi_wheres += self.class.update(self.id, { attribute => value })
+      end
+      multi_wheres
+    end
+
   end
 end
